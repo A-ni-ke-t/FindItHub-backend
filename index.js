454680e65e087dd03fe49cbc6ddc39d33d8310ed
Aniket Limaye <131771690+A-ni-ke-t@users.noreply.github.com>
2025-11-02 00:12:50 +0530

    cors fix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS BEFORE defining routes
 app.use(cors({
-    origin: "http://localhost:3000", // React app
+    origin: "http://localhost:3000" || "https://find-it-hub.vercel.app", // React app
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }));
--- sampleData.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    {
-        name: "Aniket limaye",
-        img: "https://bit.ly/2IcnSwz",
-        summary: "Found a lost mobile phone in computer lab"
-    },
-]
